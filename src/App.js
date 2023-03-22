fdb2891b23addc9bc53b5821041f8a01fe9c4f4f
gitbush-test <82594147+megi-j@users.noreply.github.com>
2023-03-22 17:34:19 +0400

    style: add header

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import styled from "styled-components";
 import { createGlobalStyle } from "styled-components";
 import { Helmet } from "react-helmet";
+import { useState } from "react";
+import { Header } from "./components/Header";
+import galleria from "./images/galleria..png";
 const GlobalStyle = createGlobalStyle`
   *{
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: 'Rubik Iso';
+    font-family: '
+    Libre Baskerville';
   }
 `;
-
+let data = require("./data.json");
 function App() {
+  const [info, setInfo] = useState(data);
+  console.log(info);
   return (
     <Container>
       <Helmet>
         <link
-          href="https://fonts.googleapis.com/css2?family=Rubik+Iso&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:wght@400;700&display=swap"
           rel="stylesheet"
         />
       </Helmet>
       <GlobalStyle />
-      hello
+      <Header>
+        <img src={galleria} alt="" />
+        <SlideShowText>START SLIDESHOW</SlideShowText>
+      </Header>
     </Container>
   );
 }
@@ -31,5 +40,10 @@ const Container = styled.div`
   max-width: 1440px;
   width: 100%;
   margin: 0 auto;
-  border: 1px solid red;
+`;
+const SlideShowText = styled.p`
+  font-weight: 700;
+  font-size: 12px;
+  letter-spacing: 2.57143px;
+  color: #7d7d7d;
 `;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+  width: 95%;
+  height: 128px;
+  margin: 0 auto;
+  border-bottom: 1px solid #e5e5e5;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
